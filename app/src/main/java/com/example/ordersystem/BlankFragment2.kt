ba76aad7572ae689087829d9da8153f81d1e0e23
星儒 林 <ace51718@gmail.com>
2022-12-21 01:06:25 +0800

    完成數量加減器

--- app/src/main/java/com/example/ordersystem/BlankFragment2.kt
+++ app/src/main/java/com/example/ordersystem/BlankFragment2.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,42 @@ class BlankFragment2 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank2, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val etAmount1: EditText = view.findViewById(R.id.etAmount1)
+        val etAmount2: EditText = view.findViewById(R.id.etAmount2)
+        val etAmount3: EditText = view.findViewById(R.id.etAmount3)
+        val etAmount4: EditText = view.findViewById(R.id.etAmount4)
+
+        val btnIncrease1: Button = view.findViewById(R.id.btnIncrease1)
+        val btnIncrease2: Button = view.findViewById(R.id.btnIncrease2)
+        val btnIncrease3: Button = view.findViewById(R.id.btnIncrease3)
+        val btnIncrease4: Button = view.findViewById(R.id.btnIncrease4)
+
+        val btnDecrease1: Button = view.findViewById(R.id.btnDecrease1)
+        val btnDecrease2: Button = view.findViewById(R.id.btnDecrease2)
+        val btnDecrease3: Button = view.findViewById(R.id.btnDecrease3)
+        val btnDecrease4: Button = view.findViewById(R.id.btnDecrease4)
+
+        btnIncrease1.setOnClickListener { calculate(etAmount1, 1) }
+        btnIncrease2.setOnClickListener { calculate(etAmount2, 1) }
+        btnIncrease3.setOnClickListener { calculate(etAmount3, 1) }
+        btnIncrease4.setOnClickListener { calculate(etAmount4, 1) }
+
+        btnDecrease1.setOnClickListener { calculate(etAmount1, -1) }
+        btnDecrease2.setOnClickListener { calculate(etAmount2, -1) }
+        btnDecrease3.setOnClickListener { calculate(etAmount3, -1) }
+        btnDecrease4.setOnClickListener { calculate(etAmount4, -1) }
+    }
+
+    private fun calculate(etAmount: EditText, num: Int) {
+        val temp = etAmount.text.toString().toInt() + num
+        if (temp >= 0){
+            etAmount.setText(temp.toString())
+        }
     }
 
     companion object {
