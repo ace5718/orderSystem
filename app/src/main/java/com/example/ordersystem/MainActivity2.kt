62fd3bba4eccf52419e9319e1b111e61584ef088
星儒 林 <ace51718@gmail.com>
2022-12-20 17:15:05 +0800

    新增 fragment 頁面 刪除 舊頁面

--- app/src/main/java/com/example/ordersystem/MainActivity2.kt
+++ app/src/main/java/com/example/ordersystem/MainActivity2.kt
@@ -11,32 +11,46 @@ import androidx.core.view.marginBottom
 import androidx.core.view.marginEnd
 import androidx.core.view.marginStart
 import androidx.core.view.marginTop
+import androidx.fragment.app.Fragment
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        val but01: Button = findViewById(R.id.but01)
         val but02: Button = findViewById(R.id.but02)
         val but03: Button = findViewById(R.id.but03)
         val but04: Button = findViewById(R.id.but04)
 
-        but02.setOnClickListener {
-            Intent(this,MainActivity3::class.java).apply {
-                startActivity(this)
+        val f1 = BlankFragment()
+        val f2 = BlankFragment2()
+        val f3 = BlankFragment3()
+        val f4 = BlankFragment4()
+
+        but01.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerView, f1)
+                commit()
             }
+        }
+
+        but02.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, f2)
+                .commit()
         }
 
         but03.setOnClickListener {
-            Intent(this,MainActivity4::class.java).apply {
-                startActivity(this)
-            }
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, f3)
+                .commit()
         }
 
         but04.setOnClickListener {
-            Intent(this,MainActivity5::class.java).apply {
-                startActivity(this)
-            }
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, f4)
+                .commit()
         }
     }
 }
